44061e795f8eeca9df3feeacaea8d733080f5102
François Blavoet <francois.blavoet@gmail.com>
2018-06-26 16:13:08 -0700

    Move from Observable to Call
    The issue I am trying to address is that some candidates feel pressured
    to use rx even if they are absolutely unfamiliar with it.
    In order to avoid setting them up for failure, we can just have a Call
    by default.
    
    If the candidate is familiar with Rx, we can easily encourage them
     to move back to Observable.

--- app/src/main/java/com/instacart/android/challenges/network/NetworkApi.java
+++ app/src/main/java/com/instacart/android/challenges/network/NetworkApi.java
@@ -1,17 +1,20 @@
 package com.instacart.android.challenges.network;
 
 import io.reactivex.Observable;
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface NetworkApi {
 
+  // Note : Feel free to use LiveData, RxJava, Coroutines, or whatever other tech works for you in order to interact with this API
+
   @GET("orders")
-  Observable<OrdersResponse> fetchOrders();
+  Call<OrdersResponse> fetchOrders();
 
   @GET("order/{order_id}")
-  Observable<OrderResponse> fetchOrderById(@Path("order_id") long id);
+  Call<OrderResponse> fetchOrderById(@Path("order_id") long id);
 
   @GET("discount/{item_id}")
-  Observable<DiscountResponse> discount(@Path("item_id") long id);
+  Call<DiscountResponse> discount(@Path("item_id") long id);
 }
