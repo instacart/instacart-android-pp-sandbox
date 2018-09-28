0194c056122c020d7fc1c72dcdd52e1fc9856796
Colin White <colin.white@instacart.com>
2018-09-28 11:05:10 -0700

    Migrate to androidx.

--- app/src/main/java/com/instacart/android/challenges/ItemAdapter.java
+++ app/src/main/java/com/instacart/android/challenges/ItemAdapter.java
@@ -1,13 +1,14 @@
 package com.instacart.android.challenges;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     static class ViewHolder extends RecyclerView.ViewHolder {
