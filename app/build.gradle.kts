649ba3ec4be14e806c0e988f6d520ffe6c9f4dfd
Mathieu Hartvick <mathieu.hartvick@instacart.com>
2025-05-13 09:07:51 -0400

    add lifecycleViewModelScope dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
