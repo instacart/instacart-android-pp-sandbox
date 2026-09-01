a5f59b75c3abe9e451134c9735243c6c7e6dc3d9
Seth Clark <seth.clark@instacart.com>
2026-09-01 16:21:54 -0400

    More renaming

--- app/src/main/java/com/instacart/android/challenges/network/NetworkApi.kt
+++ app/src/main/java/com/instacart/android/challenges/network/NetworkApi.kt
@@ -8,22 +8,22 @@ import retrofit2.http.Path
 interface NetworkApi {
 
     @GET("order/{order_id}")
-    fun fetchOrderItems(@Path("order_id") id: Long): Call<OrderResponse>
+    fun fetchOrderItems(@Path("order_id") id: Long): Call<OrderItemsResponse>
 
     @GET("order/{order_id}")
-    fun fetchOrderItemsObservable(@Path("order_id") id: Long): Observable<OrderResponse>
+    fun fetchOrderItemsObservable(@Path("order_id") id: Long): Observable<OrderItemsResponse>
 
     @GET("order/{order_id}")
-    suspend fun fetchOrderItemsCoroutine(@Path("order_id") id: Long): OrderResponse
+    suspend fun fetchOrderItemsCoroutine(@Path("order_id") id: Long): OrderItemsResponse
 
     @GET("orders")
-    fun fetchOrderIds(): Call<OrdersResponse>
+    fun fetchOrderIds(): Call<OrderIdsResponse>
 
     @GET("orders")
-    fun fetchOrderIdsObservable(): Observable<OrdersResponse>
+    fun fetchOrderIdsObservable(): Observable<OrderIdsResponse>
 
     @GET("orders")
-    suspend fun fetchOrderIdsCoroutine(): OrdersResponse
+    suspend fun fetchOrderIdsCoroutine(): OrderIdsResponse
 
     @GET("price/{item_id}")
     fun price(@Path("item_id") id: Long): Call<PriceResponse>
